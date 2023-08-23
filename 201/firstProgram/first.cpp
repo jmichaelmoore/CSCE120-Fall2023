fecbfc4fe0afe773c3ac10dd8b5cd575f09267a0
jmichaelmoore <jmichael@tamu.edu>
2023-08-23 10:01:59 -0500

    class

--- 201/firstProgram/first.cpp
+++ 201/firstProgram/first.cpp
@@ -1,1 +1,11 @@
-//
+#include <iostream>
+
+// using namespace std; // not preferred
+using std::cout, std::cin, std::endl;
+
+int main() {
+    std::string name;
+    cout << "What is your name? ";
+    cin >> name;
+    cout << "Gig Em " << name << "!" << std::endl;
+}
---
+++ 201/prereq1/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+double largestNum(double, double, double);
+
+int main() {
+    double val1 = 0;
+    double val2 = 0;
+    double val3 = 0;
+    cin >> val1 >> val2 >> val3;
+    cout << "Largest: ";
+    cout << largestNum(val1, val2, val3);
+    cout << endl;
+}
+
+double largestNum(double a, double b, double c) {
+  if ( a > b && a > c) {
+        return a;
+    } else if (b > c) {
+        return b;
+    }
+    // else
+    return c;
+}
