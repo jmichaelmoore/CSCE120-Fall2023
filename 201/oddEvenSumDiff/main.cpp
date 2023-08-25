8f7b4a777bc0ce8082e4589b07e91387e230fe96
jmichaelmoore <jmichael@tamu.edu>
2023-08-25 08:42:17 -0500

    prep

--- 201/oddEvenSumDiff/main.cpp
+++ 201/oddEvenSumDiff/main.cpp
@@ -1,1 +1,7 @@
-//
+#include <iostream>
+
+using std::cout, std::cin, std::endl; // need C++ or higher for this line, otherwise put each on separate line with semicolons at end
+
+int main() {
+    
+}
