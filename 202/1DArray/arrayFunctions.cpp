76609e1ff925be58372634c7b5b2c40ee4253e7f
Michael Moore <jmichael@tamu.edu>
2023-08-30 11:35:59 -0500

    update

--- 202/1DArray/arrayFunctions.cpp
+++ 202/1DArray/arrayFunctions.cpp
@@ -7,11 +7,19 @@ using std::out_of_range;
 
 // always throw an out_of_range exception for any invalid index
 
-void loadRandom(int ary[], int size) {
+void loadRandom(int ary[], unsigned int size) {
+  for (unsigned int i=0; i<size; ++i) {
+    ary[i] = rand()%1000;
+  }
 }
 
 void insert(int val, unsigned int index, 
           int ary[], unsigned int size) {
+  // assume index <= size
+  // assume size < capacity
+  for (unsigned int i=0; i<(size-index); ++i) {
+    ary[size-i] = ary[size-i-1];
+  }
 }
 
 void removeAtIndex(unsigned int index, 
@@ -22,16 +30,26 @@ void removeFirstOf(int val,
           int ary[], unsigned int size) {
 }
 
-int getMax(int ary[], unsigned int size) {
+int getMax(const int ary[], unsigned int size) {
   return 0;
 }
 
-int getMin(int ary[], unsigned int size) {
+int getMin(const int ary[], unsigned int size) {
   return 0;
 }
 
-unsigned int countVal(int val, int ary[], unsigned int size) {
+unsigned int countVal(int val, const int ary[], unsigned int size) {
   return 0;
 }
 
 // print including empty
+void print(const int ary[], unsigned int size) {
+  if (size == 0) {
+    cout << "Empty array" << endl;
+  }
+  else {
+    for (unsigned int i = 0; i < size; ++i) {
+      cout << ary[i] << endl;
+    }
+  }
+}
