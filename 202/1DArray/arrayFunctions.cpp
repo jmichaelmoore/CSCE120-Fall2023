4fb455fdd654e9f085793b885789b9da6eb9cd73
J. Michael Moore <jmichael@tamu.edu>
2023-08-30 08:45:00 -0500

    prep

---
+++ 202/1DArray/arrayFunctions.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "arrayFunctions.h"
+
+using std::cin, std::cout, std::endl;
+
+using std::out_of_range;
+
+// always throw an out_of_range exception for any invalid index
+
+void loadRandom(int ary[], int size) {
+}
+
+void insert(int val, unsigned int index, 
+          int ary[], unsigned int size) {
+}
+
+void removeAtIndex(unsigned int index, 
+          int ary[], unsigned int size) {
+}
+
+void removeFirstOf(int val, 
+          int ary[], unsigned int size) {
+}
+
+int getMax(int ary[], unsigned int size) {
+  return 0;
+}
+
+int getMin(int ary[], unsigned int size) {
+  return 0;
+}
+
+unsigned int countVal(int val, int ary[], unsigned int size) {
+  return 0;
+}
+
+// print including empty
