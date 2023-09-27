1e6eb36acc7bfce96f9e542c2917331b8d2fecd0
Michael Moore <jmichael@tamu.edu>
2023-09-27 11:11:17 -0500

    9/27 lectures

--- 201/streams/unnecessaryCensorship/censor.cpp
+++ 201/streams/unnecessaryCensorship/censor.cpp
@@ -3,22 +3,43 @@
 #include <string>
 #include <sstream>
 
-using std::cin, std::cout, std::endl, std::string;
+using std::cin, std::cout, std::cerr, std::endl, std::string;
 using std::istringstream, std::ifstream, std::ofstream;
 
 int main() {
 	// Get filenames - For now we will hard code the names
 	string censorWordsFilename = "censorwords.txt";
-	string originalTextFilename = "original.txt";
+	string originalTextFilename = "frozen.txt";
 	string censoredTextFilename = "censored.txt";
 
 	// Declare/Define/Open filestreams
+	ifstream censorWdsIn(censorWordsFilename);
+	ifstream originalIn{originalTextFilename};
+	ofstream censorOut = ofstream(censoredTextFilename);
 
 	// make sure all files were opened and give feedback if not
+	if (!censorWdsIn.is_open()) {
+		cerr << "Unable to open file: " << censorWordsFilename << endl;
+		return 1;
+	}
+	if (!originalIn.is_open()) {
+		cerr << "Unable to open file: " << originalTextFilename << endl;
+		return 1;
+	}
+	if (!censorOut.is_open()) {
+		cerr << "Unable to open file: " << censoredTextFilename << endl;
+		return 1;
+	}
 
 	// get list of words to censor from file
+	string censorWds;
+	string censorWd;
+	while(censorWdsIn >> censorWd) {
+		censorWds = censorWds + censorWd + " ";
+	}
 
 	// print out list of censored words (for debugging) remove later from final program
+	cout << "censorWds: " << censorWds << endl;
 
 	// read original, process and send to output
 	// process line by line
@@ -26,4 +47,18 @@ int main() {
 		// see if each censor word is in line
 		// if it is there, replace with symbols
 		// output modified line
+	while (!originalIn.eof()) {
+		string line;
+		getline(originalIn, line);
+		istringstream cwds(censorWds);
+		string wd;
+		while (cwds >> wd) {
+			unsigned int location = 0;
+			do {
+				location = line.find(wd);
+				//start here!!!!
+			} while (location != string::npos);
+		}
+		censorOut << line;
+	}
 }
--- 202/streams/unnecessaryCensorship/censor.cpp
+++ 202/streams/unnecessaryCensorship/censor.cpp
@@ -3,22 +3,43 @@
 #include <string>
 #include <sstream>
 
-using std::cin, std::cout, std::endl, std::string;
+using std::cin, std::cout, std::cerr, std::endl, std::string;
 using std::istringstream, std::ifstream, std::ofstream;
 
 int main() {
 	// Get filenames - For now we will hard code the names
 	string censorWordsFilename = "censorwords.txt";
-	string originalTextFilename = "original.txt";
+	string originalTextFilename = "frozen.txt";
 	string censoredTextFilename = "censored.txt";
 
 	// Declare/Define/Open filestreams
+	ifstream censorWdsIn = ifstream(censorWordsFilename);
+	ifstream originalIn(originalTextFilename);
+	ofstream censorOut{censoredTextFilename};
 
 	// make sure all files were opened and give feedback if not
+	if (!censorWdsIn.is_open()) {
+		cerr << "Unable to open file " << censorWordsFilename << endl;
+		return 1;
+	}
+	if (!originalIn.is_open()) {
+		cerr << "Unable to open file " << originalTextFilename << endl;
+		return 1;
+	}
+	if (!censorOut.is_open()) {
+		cerr << "Unable to open file " << censoredTextFilename << endl;
+		return 1;
+	}
 
 	// get list of words to censor from file
+	string censorWds;
+	string wd;
+	while (censorWdsIn >> wd) {
+		censorWds = censorWds + wd + " ";
+	}
 
 	// print out list of censored words (for debugging) remove later from final program
+	cout << "censorWds: " << censorWds << endl;
 
 	// read original, process and send to output
 	// process line by line
@@ -26,4 +47,17 @@ int main() {
 		// see if each censor word is in line
 		// if it is there, replace with symbols
 		// output modified line
+	while (!originalIn.eof()) {
+		string line;
+		getline(originalIn, line);
+		istringstream issCensor(censorWds);
+		string wd;
+		while (issCensor >> wd) {
+			unsigned int location = line.find(wd);
+			if (location != string::npos) {
+				// start here replace word
+			}
+		}
+		censorOut << line << endl;
+	}
 }
