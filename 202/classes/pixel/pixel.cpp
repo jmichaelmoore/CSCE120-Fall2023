b7cb0e08bd279e2d5eee685c868fb347140fb116
Michael Moore <jmichael@tamu.edu>
2023-10-26 18:29:04 -0500

    class 10/25

--- 202/classes/pixel/pixel.cpp
+++ 202/classes/pixel/pixel.cpp
@@ -1,16 +1,17 @@
 #include <stdexcept>
+
 class Pixel {
-private: // optional here since classes are private by default
+private: // optional since classes are private by default
     unsigned short red;
     unsigned short green;
     unsigned short blue;
 public:
-    Pixel (unsigned short red, unsigned short green, unsigned short blue) : 
+    Pixel(unsigned short red, unsigned short green, unsigned short blue) :
         red(red), green(green), blue(blue) {
-            if (red>255 || green>255 || blue>255) {
+            if (red>255 or green>255 or blue>255) {
                 throw std::invalid_argument("Color values must be 255 or less.");
             }
-        };
+        }
     Pixel(const Pixel& p) : red(p.red), green(p.green), blue(p.blue) {} // copy constructor
     Pixel() : red(0), green(0), blue(0) {} // default constructor
 };
